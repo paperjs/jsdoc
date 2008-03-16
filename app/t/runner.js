9efbf8125ae183b6c8a2b60dc465d65ac588525c
micmath <micmath@c4b26cf2-672e-0410-81b6-737671b986b9>
2008-03-16 23:11:11 +0000

    git-svn-id: http://jsdoc-toolkit.googlecode.com/svn/branches/version2.0@536 c4b26cf2-672e-0410-81b6-737671b986b9

--- app/t/runner.js
+++ app/t/runner.js
@@ -1,6 +1,7 @@
 load("TestDoc.js");
 
-//TestDoc.add("../frame/String.js");
+TestDoc.add("../frame/String.js");
 TestDoc.add("../lib/JSDOC/DocTag.js");
-//TestDoc.add("../lib/JSDOC/TokenReader.js");
+TestDoc.add("../lib/JSDOC/TokenReader.js");
+
 TestDoc.report();
