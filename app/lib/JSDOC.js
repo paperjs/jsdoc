a81bfcfd06d25531b3fb0f33bb224117009aebec
micmath <micmath@c4b26cf2-672e-0410-81b6-737671b986b9>
2008-05-29 21:53:56 +0000

    Updated version number.
    git-svn-id: http://jsdoc-toolkit.googlecode.com/svn/branches/version2.0@612 c4b26cf2-672e-0410-81b6-737671b986b9

--- app/lib/JSDOC.js
+++ app/lib/JSDOC.js
@@ -28,7 +28,7 @@ JSDOC.opt = Opt.get(
 );
 
 /** The current version string of this application. */
-JSDOC.VERSION = "2.0.0";
+JSDOC.VERSION = "2.0.1";
 
 /** Print out usage information and quit. */
 JSDOC.usage = function() {
