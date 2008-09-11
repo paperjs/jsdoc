c697e8193f16b989bbf3dc65d7fc87e820e1bde3
micmath <micmath@c4b26cf2-672e-0410-81b6-737671b986b9>
2008-09-11 21:49:46 +0000

    Added better comments. Added check to see if templates path ends in a slash. (issue #177)
    git-svn-id: http://jsdoc-toolkit.googlecode.com/svn/trunk@685 c4b26cf2-672e-0410-81b6-737671b986b9

--- app/main.js
+++ app/main.js
@@ -6,27 +6,42 @@ function main() {
 	IO.include("lib/JSDOC.js");
 	IO.includeDir("plugins/");
 	
+	// be verbose
 	if (JSDOC.opt.v) LOG.verbose = true;
+	
+	// send log messages to a file
 	if (JSDOC.opt.o) LOG.out = IO.open(JSDOC.opt.o);
 	
+	// run the unit tests
 	if (JSDOC.opt.T) {
 		LOG.inform("JsDoc Toolkit running in test mode at "+new Date()+".");
 		IO.include("frame/Testrun.js");
 		IO.include("test.js");
 	}
 	else {
+		// a template must be defined and must be a directory path
+		if (!JSDOC.opt.t && System.getProperty("jsdoc.template.dir")) {
+			JSDOC.opt.t = System.getProperty("jsdoc.template.dir");
+		}
+		if (JSDOC.opt.t && !JSDOC.opt.t.charAt(JSDOC.opt.t.length-1).match(/[\\\/]/)) {
+			JSDOC.opt.t += SYS.slash;
+		}
+		
+		// verbose messages about the options we were given
 		LOG.inform("JsDoc Toolkit main() running at "+new Date()+".");
 		LOG.inform("With options: ");
 		for (var o in JSDOC.opt) {
 			LOG.inform("    "+o+": "+JSDOC.opt[o]);
 		}
 		
-		var jsdoc = new JSDOC.JsDoc();
+		// initialize and build a symbolSet from your code
+		JSDOC.JsDoc();
 		
-		if (JSDOC.opt.Z) { // secret debugging option
+		// debugger's option: dump the entire symbolSet produced from your code
+		if (JSDOC.opt.Z) {
 			LOG.warn("So you want to see the data structure, eh? This might hang if you have circular refs...");
 			IO.include("frame/Dumper.js");
-			var symbols = jsdoc.symbolSet.toArray();
+			var symbols = JSDOC.JsDoc.symbolSet.toArray();
 			for (var i = 0, l = symbols.length; i < l; i++) {
 				var symbol = symbols[i];
 				print("// symbol: " + symbol.alias);
@@ -34,39 +49,37 @@ function main() {
 			}
 		}
 		else {
-			var template = JSDOC.opt.t || System.getProperty("jsdoc.template.dir");
-
-			var handler = jsdoc.symbolSet.handler;
-			if (handler && handler.publish) {
-				handler.publish(jsdoc.symbolSet);
+			if (typeof JSDOC.opt.t != "undefined") {
+				try {
+					// a file named "publish.js" must exist in the template directory
+					load(JSDOC.opt.t+"publish.js");
+					
+					// and must define a function named "publish"
+					if (!publish) {
+						LOG.warn("No publish() function is defined in that template so nothing to do.");
+					}
+					else {
+						// which will be called with the symbolSet produced from your code
+						publish(JSDOC.JsDoc.symbolSet);
+					}
+				}
+				catch(e) {
+					LOG.warn("Sorry, that doesn't seem to be a valid template: "+JSDOC.opt.t+"publish.js : "+e);
+				}
 			}
 			else {
-				if (typeof template != "undefined") {
-					try {
-						load(template+"/publish.js");
-						if (!publish) {
-							LOG.warn("No publish() function is defined in that template so nothing to do.");
-						}
-						else {
-							publish(jsdoc.symbolSet);
-						}
-					}
-					catch(e) {
-						LOG.warn("Sorry, that doesn't seem to be a valid template: "+template+"/publish.js : "+e);
-					}
-				}
-				else {
-					LOG.warn("No template or handlers given. Might as well read the usage notes.");
-					JSDOC.usage();
-				}
+				LOG.warn("No template or handlers given. Might as well read the usage notes.");
+				JSDOC.usage();
 			}
 		}
 	}
 	
+	// notify of any warnings
 	if (LOG.warnings.length) {
 		print(LOG.warnings.length+" warning"+(LOG.warnings.length != 1? "s":"")+".");
 	}
 	
+	// stop sending log messages to a file
 	if (LOG.out) {
 		LOG.out.flush();
 		LOG.out.close();
