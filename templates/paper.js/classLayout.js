5c3c626aad4fc1a0698bd3e4d29af34465074cae
Jürg Lehni <juerg@scratchdisk.com>
2011-06-14 23:03:21 +0100

    Add PathItem to class layout.

--- templates/paper.js/classLayout.js
+++ templates/paper.js/classLayout.js
@@ -2,7 +2,7 @@ var classLayout = {
 	'_global_': ['Global Scope:_global_'],
 	'Basic Types': ['Point', 'Size', 'Rectangle', 'Matrix'],
 	'Items': ['Item', 'Layer', 'Group', 'Raster', 'PlacedSymbol'],
-	'Paths': ['Path', 'CompoundPath', 'Segment', 'Curve', 'PathStyle', 'ruler',
+	'Paths': ['PathItem', 'Path', 'CompoundPath', 'Segment', 'Curve', 'PathStyle', 'ruler',
 		'CurveLocation'],
 	'Tools': ['Tool', 'ToolEvent', 'Key', 'KeyEvent'],
 	'Projects': ['Project', 'View', 'Symbol'],
