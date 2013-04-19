64fbc41039883d17848089086f66a89c1a6a0079
Jürg Lehni <juerg@scratchdisk.com>
2013-04-19 11:53:43 -0700

    Restructure class sequence.

--- templates/paper.js/classLayout.js
+++ templates/paper.js/classLayout.js
@@ -1,13 +1,13 @@
 var classLayout = {
 	'_global_': ['Global Scope:_global_'],
 	'Basic Types': ['Point', 'Size', 'Rectangle', 'Matrix'],
+	'Projects': ['Project', 'Symbol'],
 	'Items': ['Item', 'Layer', 'Group', 'Raster', 'PlacedSymbol',
 		'ruler', 'HitResult'],
 	'Paths': ['PathItem', 'Path', 'CompoundPath', 'Segment', 'Curve',
 		'ruler', 'CurveLocation'],
-	'Typography': ['TextItem', 'PointText' ],
 	'Styling': ['Style', 'Color', 'Gradient', 'GradientStop'],
-	'Projects': ['Project', 'Symbol'],
+	'Typography': ['TextItem', 'PointText' ],
 	'User Interfaces': ['View', 'Tool', 'ToolEvent', 'Palette', 'Component',
 		'Key', 'KeyEvent'],
 	'JavaScript': ['PaperScope', 'PaperScript']
