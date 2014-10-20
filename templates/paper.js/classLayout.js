f74c88830caf74b13af82f48abc7342643c668fd
Jürg Lehni <juerg@scratchdisk.com>
2014-10-20 21:27:09 +0200

    Update class layout.

--- templates/paper.js/classLayout.js
+++ templates/paper.js/classLayout.js
@@ -1,14 +1,13 @@
 var classLayout = {
 	'_global_': ['Global Scope:_global_'],
 	'Basic Types': ['Point', 'Size', 'Rectangle', 'Matrix'],
-	'Projects': ['Project', 'Symbol'],
+	'Projects': ['Project', 'View', 'Symbol'],
 	'Items': ['Item', 'Layer', 'Group', 'Shape', 'Raster', 'PlacedSymbol',
 		'ruler', 'HitResult'],
 	'Paths': ['PathItem', 'Path', 'CompoundPath', 'Segment', 'Curve',
 		'ruler', 'CurveLocation'],
 	'Styling': ['Style', 'Color', 'Gradient', 'GradientStop'],
 	'Typography': ['TextItem', 'PointText' ],
-	'User Interfaces': ['View', 'Tool', 'ToolEvent', 'Palette', 'Component',
-		'Key', 'KeyEvent'],
+	'User Interface & Events': ['Tool', 'ToolEvent', 'Key', 'KeyEvent'],
 	'JavaScript': ['PaperScope', 'PaperScript']
 };
