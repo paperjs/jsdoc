5f625317408fa6aea342af59cb4df4ea47f07c66
Jürg Lehni <juerg@scratchdisk.com>
2016-01-08 20:31:20 +0100

    Update class hierarchy for Paper.js

--- templates/paper.js/classLayout.js
+++ templates/paper.js/classLayout.js
@@ -1,13 +1,28 @@
 var classLayout = {
-	'_global_': ['Global Scope:_global_'],
-	'Basic Types': ['Point', 'Size', 'Rectangle', 'Matrix'],
-	'Projects': ['Project', 'View', 'Symbol'],
-	'Items': ['Item', 'Layer', 'Group', 'Shape', 'Raster', 'PlacedSymbol',
-		'ruler', 'HitResult'],
-	'Paths': ['PathItem', 'Path', 'CompoundPath', 'Segment', 'Curve',
-		'ruler', 'CurveLocation'],
-	'Styling': ['Style', 'Color', 'Gradient', 'GradientStop'],
-	'Typography': ['TextItem', 'PointText' ],
-	'User Interface & Events': ['Tool', 'ToolEvent', 'Key', 'KeyEvent'],
-	'JavaScript': ['PaperScope', 'PaperScript']
+	'_global_': [
+		'Global Scope:_global_'
+	],
+	'Basic Types': [
+		'Point', 'Size', 'Rectangle', 'Matrix'
+	],
+	'Project & Items': [
+		'Project', 'Item', 'Layer', 'Group', 'Shape', 'Raster', 'Symbol',
+		'PlacedSymbol', 'ruler', 'HitResult'
+	],
+	'Paths': [
+		'PathItem', 'Path', 'CompoundPath', 'Segment', 'Curve', 'ruler',
+		'CurveLocation'
+	],
+	'Styling': [
+		'Style', 'Color', 'Gradient', 'GradientStop'
+	],
+	'Typography': [
+		'TextItem', 'PointText'
+	],
+	'User Interaction & Events': [
+		'View', 'Event', 'MouseEvent', 'Tool', 'ToolEvent', 'Key', 'KeyEvent'
+	],
+	'JavaScript': [
+		'PaperScope', 'PaperScript'
+	]
 };
