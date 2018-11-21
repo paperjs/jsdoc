f1264de739f43c4b11a02134a4c4ab3283a6623b
sasensi <asensi.samuel@gmail.com>
2018-11-21 12:18:20 +0100

    Add typescript definition generation
    This add a gulp task (`gulp docs:typescript`) to automatically generate
    a typescript definition for the library. This should solve the problem
    of having an out of sync type definition when we change the API.
    
    This task takes advantage of existing JSDoc parsing to generate a
    temporary file which is later formatted through a mustache template to
    generate the final definition. This definition is then tested by
    compiling a typescript file that use it.
    The generated definition is added to the `gulp zip` task in order to be
    published along with the bundled library.
    
    So 2 new dev-dependencies are added with this change: `mustache` and
    `typescript` packages. Using node and mustache to generate the
    definition instead of relying on existing templating system is
    motivated by a better development experience, with easier debugging
    possibilities... through the usage of more modern tools.
    
    As a side note, support of "rest parameters" (when a parameter can be
    present multiple times) is added to existing JSDoc parser in order to
    support this pattern on typescript side (E.g. for `Color#set()` method
    which accept any sequence of parameters that is supported by `Color`
    constructors).

--- templates/typescript/publish.js
+++ templates/typescript/publish.js
@@ -1,91 +1,55 @@
-﻿// Code taken and adapted from https://github.com/joshheyse/jsdocts
-
+﻿/**
+ * This publishes raw symbols datas to a file which will later be proceeded by
+ * another script (see typescript-definition-generator.js).
+ */
 function publish(symbolSet) {
+	// Filter and organize JSDOC parsed data.
+	var symbols = symbolSet.toArray().sort(makeSortby('alias'));
+	var classes = symbols.filter(isClass).filter(isVisible);
+	var global = symbols.filter(isGlobal)[0];
 
-    var outPath = JSDOC.opt.o;
-    
-    var templatesDir = JSDOC.opt.t || SYS.pwd+"../templates/jsdoc/";
-    var classTemplate = new JSDOC.JsPlate(templatesDir + "module.tmpl");
+	// Build a data object which will be used by next script.
+	var result = {
+		classes: classes,
+		global: global,
+		version: JSDOC.opt.D.version,
+		date: JSDOC.opt.D.date
+	};
 
-    function isaClass($) { return (($.is("CONSTRUCTOR") || $.isNamespace) && ($.alias != "_global_")); }
-    
-    function mapEvent($) {
-        return {
-            name: $.name,
-            isPrivate: $.isPrivate,
-            isStatic: $.isStatic
-        };
-    }
-    
-    function mapMethod($) {
-        return {
-            name: $.name,
-            isPrivate: $.isPrivate,
-            isStatic: $.isStatic,
-            params: $.params.map(function (_) { return { name: _.name, type: _.type }; }),
-            returnType: $.type || $.returns[0] || "any"
-        };
-    }
+	// Write it in a file.
+	var file = JSDOC.opt.D.file;
+	var directory = FilePath.dir(JSDOC.opt.D.file);
+	IO.mkPath(directory);
+	IO.saveFile(directory, FilePath.fileName(file), JSON.stringify(result));
 
-    function mapProperty($) {
-        return {
-            name: $.name,
-            isPrivate: $.isPrivate,
-            isStatic: $.isStatic,
-            type: $.type || $.returns[0] || "any"
-        };
-    }
 
-    var symbols = symbolSet.toArray().sort(makeSortby("alias"));
-    var classes = symbols.filter(isaClass).sort(makeSortby("alias"));
+	// Utility methods
 
-    var modules = {};
+	function isClass(_) {
+		return ((_.is('CONSTRUCTOR') || _.isNamespace) && (_.alias != '_global_'));
+	}
 
-    for (var i = 0; i < classes.length; i++) {
-        var symbol = classes[i];
-        if (!symbol.isVisible())
-            continue;
-        var moduleName = symbol.memberOf || "{GLOBAL}";
-        var module = modules[moduleName] = modules[moduleName] || { "classes" : []};
-        module.name = moduleName;
-        var cls = {
-            name: symbol.name,
-            inheritsFrom: symbol.inheritsFrom.getUnique(),
-            events: symbol.getEvents().filter(function ($) { return $.memberOf == symbol.alias && !$.isNamespace; }).map(mapEvent),
-            methods: symbol.getMethods().filter(function ($) { return $.memberOf == symbol.alias && !$.isNamespace; }).map(mapMethod),
-            properties: symbol.properties.filter(function ($) { return $.memberOf == symbol.alias && !$.isNamespace; }).map(mapProperty)
-        };
-        module.classes.push(cls);
-    }
-    var output = classTemplate.process(modules);
-    IO.saveFile(FilePath.dir(outPath), FilePath.fileName(outPath), output);
-}
+	function isGlobal(_) {
+		return _.isNamespace && _.alias == '_global_' && _.properties.length > 0;
+	}
 
-Array.prototype.getUnique = function () {
-    var u = {}, a = [];
-    for (var i = 0, l = this.length; i < l; ++i) {
-        if (u.hasOwnProperty(this[i])) {
-            continue;
-        }
-        a.push(this[i]);
-        u[this[i]] = 1;
-    }
-    return a.length > 0 ? a : null;
-};
+	function isVisible(_) {
+		return _.isVisible();
+	}
 
-function makeSortby(attribute) {
-    return function(a, b) {
-        if (a[attribute] != undefined && b[attribute] != undefined) {
-            a = a[attribute].toLowerCase();
-            b = b[attribute].toLowerCase();
-            if (a < b) return -1;
-            if (a > b) return 1;
-            return 0;
-        }
-    }
-}
-
-function makeSignature(params) {
-    if (!params) return "()";
-    return "(" + params.map(function ($) { return $.name + ($.type ? ": " + $.type : ""); }).join(", ") + ")";
+	function makeSortby(attribute) {
+		return function(a, b) {
+			if (a[attribute] != undefined && b[attribute] != undefined) {
+				a = a[attribute].toLowerCase();
+				b = b[attribute].toLowerCase();
+				if (a < b) {
+					return -1;
+				}
+				if (a > b) {
+					return 1;
+				}
+				return 0;
+			}
+		};
+	}
 }
